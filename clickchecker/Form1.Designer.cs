0ba4c565ef45d6c7b75a3f4632428c9aa46cffae
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-07-13 23:16:42 +0000

    start
    git-svn-id: svn://192.168.3.99/clickchecker@7130 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ clickchecker/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace clickchecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelClick = new System.Windows.Forms.Panel();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panelClick
+            // 
+            this.panelClick.BackColor = System.Drawing.Color.Blue;
+            this.panelClick.Location = new System.Drawing.Point(12, 12);
+            this.panelClick.Name = "panelClick";
+            this.panelClick.Size = new System.Drawing.Size(427, 100);
+            this.panelClick.TabIndex = 0;
+            this.panelClick.DoubleClick += new System.EventHandler(this.panelClick_DoubleClick);
+            this.panelClick.Paint += new System.Windows.Forms.PaintEventHandler(this.panelClick_Paint);
+            this.panelClick.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.panelClick_MouseDoubleClick);
+            this.panelClick.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panelClick_MouseClick);
+            this.panelClick.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelClick_MouseDown);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 170);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(558, 171);
+            this.txtLog.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 372);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.panelClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelClick;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ clickchecker/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace clickchecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panelClick_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panelClick_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panelClick_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelClick_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Logit("double",e);
+        }
+
+        int linecount_;
+        private void panelClick_MouseDown(object sender, MouseEventArgs e)
+        {
+            Logit("single",e);
+        }
+
+        private void Logit(string clicktype, MouseEventArgs e)
+        {
+            linecount_++;
+            StringBuilder line = new StringBuilder();
+            line.Append(linecount_);
+            line.Append(": ");
+            line.Append(clicktype);
+            line.Append(e.Button.ToString());
+            line.AppendLine();
+            txtLog.AppendText(line.ToString());
+        }
+    }
+}
